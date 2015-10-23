40d1f9b1ecef3ca95342f08e594a3d9cce3c062d
kolonse <KolonseTiger@gmail.com>
2015-10-23 18:56:00 +0800

    修改logo图片

--- KolonseBlogWeb.go
+++ KolonseBlogWeb.go
@@ -3,15 +3,12 @@ package main
 
 import (
 	"github.com/kolonse/KolonseWeb"
-	"github.com/kolonse/KolonseWeb/HttpLib"
-	"github.com/kolonse/KolonseWeb/Type"
+	//	"github.com/kolonse/KolonseWeb/HttpLib"
+	//	"github.com/kolonse/KolonseWeb/Type"
 	"github.com/kolonse/KolonseWeb/middleWare/StaticDir"
 )
 
 func main() {
 	KolonseWeb.DefaultApp.Use(StaticDir.NewMiddleWare("Views"))
-	KolonseWeb.DefaultApp.Get("/", func(req *HttpLib.Request, res *HttpLib.Response, next Type.Next) {
-		KolonseWeb.BeeLogger.Debug("recv /")
-	})
 	KolonseWeb.DefaultApp.Listen("0.0.0.0", *Port)
 }
